63e5cd9b83b5871f2996c9a56a8d675ca56c5ba1
Ryan Permana <ryan.permana@gmail.com>
2020-03-17 21:48:28 +0700

    create model fetch api ke node users:

--- app/routes/test.js
+++ app/routes/test.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 export default class TestRoute extends Route {
+  model() {
+    let url = 'http://localhost:3000/users';
+    return fetch(url).then(function (response) {
+      console.log(response)
+      return response.json();
+    });
+  }
 }
